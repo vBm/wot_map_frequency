0e936bf5a5c1508b5268d7ed81992dd3b3d24f2e
vBm <the.vbm@gmail.com>
2016-06-05 13:01:07 +0200

    bump to v0.3

--- helpers/__init__.py
+++ helpers/__init__.py
@@ -1,4 +1,4 @@
-version = '0.2'
+version = '0.3'
 name = 'wot_map_frequency'
 description = 'Basic Python script that counts map frequency based on replays'
 projectURL = 'https://github.com/vBm/wot_map_frequency'
